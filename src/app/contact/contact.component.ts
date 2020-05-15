254eab52c6678aefa0a4dc0b2f0c6173f9e559fd
mohitpatell <mohit@wednesday.is>
2020-05-15 16:54:34 +0530

    ruc contact form done

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  submitted: boolean = false;
+  constructor(public http: HttpClient) { }
 
   ngOnInit() {
+    this.form = new FormGroup ({
+      name: new FormControl(null,{validators: [Validators.required]}),
+      email: new FormControl(null,{validators: [Validators.required]}),
+      subject: new FormControl(null,{validators: [Validators.required]}),
+      message: new FormControl(null,{validators: [Validators.required]}),
+    })
+  }
+
+  contact() {
+    this.submitted = true;
+    if(this.form.invalid) return;
+    console.log(this.form.value)
+    this.http.post<{status:string, msg:string, result:any}>('https://onewater-auth.herokuapp.com/ruc-contact', this.form.value)
+    .subscribe(result=> {
+      alert(result.result)
+    })
   }
 
 }
