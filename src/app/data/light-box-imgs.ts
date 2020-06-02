6526b0f72ad2358b1fb06a7398fd82ab956ff546
mandeepb29 <mandeepb29@gmail.com>
2020-06-03 00:24:59 +0530

    about images updated

--- src/app/data/light-box-imgs.ts
+++ src/app/data/light-box-imgs.ts
@@ -5,6 +5,17 @@ import { Injectable } from "@angular/core";
 })
 export class Images {
   aboutImgs = [
+  
+  {
+    src:'assets/img/about/IMG_4012.jpg',
+    thumb:'assets/img/about/IMG_4012_thumb.jpg',
+    caption:'IMG_4012'
+},
+{
+  src:'assets/img/about/IMG_4129.jpg',
+  thumb:'assets/img/about/IMG_4129_thumb.jpg',
+  caption:'IMG_4129'
+},
       {
           src:'assets/img/about/IMG_4113.jpg',
           thumb:'assets/img/about/IMG_4113_thumb.jpg',
@@ -21,9 +32,15 @@ export class Images {
         caption:'IMG_4189'
     },
     {
+      src:'assets/img/about/Alice.jpg',
+      thumb:'assets/img/about/Alice_thumb.jpg',
+      caption:'Alice'
+  },
+    {
         src:'assets/img/about/IMG_4259.jpg',
         thumb:'assets/img/about/IMG_4259_thumb.jpg',
         caption:'IMG_4259'
-    }
+    },
+
   ]
 }
